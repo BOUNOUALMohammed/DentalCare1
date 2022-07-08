4e4966ae9f88dcf0aed431858bc9ce542f49d047
Mohammed <bounoual1999@gmail.com>
2022-07-08 11:07:13 +0100

    First Commit

---
+++ src/main/java/ma/emsi/dentalcare1/DentalCare1Application.java
@@ -0,0 +1,13 @@
+package ma.emsi.dentalcare1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalCare1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalCare1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ma/emsi/dentalcare1/DentalCare1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.dentalcare1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalCare1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
